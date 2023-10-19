a5155ee8a84db6049f6b05f03c3a2269df84eec9
samegener <thomaf0107@yahoo.no>
2023-10-18 20:23:03 -0700

    add python solver api

---
+++ src/scripts/sudoku.js
@@ -0,0 +1,37 @@
+export async function solveSudoku() {
+    
+    const table = document.querySelector("table");
+    let board = [];
+    for (let row of table.rows) {
+        let currentRow = [];
+        for (let cell of row.cells) {
+            currentRow.push(Number(cell.firstChild.value) || 0);
+        }
+        board.push(currentRow);
+    }
+
+    try {
+        // Adjust the following line to point to your PythonAnywhere subdomain
+        let response = await fetch("https://yourusername.pythonanywhere.com/solve", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ board: board })
+        });
+
+        let data = await response.json();
+        
+        if (data.status === "success") {
+            for (let i = 0; i < 9; i++) {
+                for (let j = 0; j < 9; j++) {
+                    table.rows[i].cells[j].firstChild.value = data.board[i][j];
+                }
+            }
+        } else {
+            alert(data.message);
+        }
+    } catch (error) {
+        alert("Failed to solve. Check your input or try again later.");
+    }
+}
