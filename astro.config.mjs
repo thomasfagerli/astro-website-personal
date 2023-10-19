a15422f7b963ff702dc50df6c83b63f886ff3660
samegener <thomaf0107@yahoo.no>
2023-10-18 22:58:19 -0700

    forcereload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    buildOptions: {
-        // Disable automatic page prefetching
-        pageStrategy: 'none',
-      },
-});
+export default defineConfig({});
